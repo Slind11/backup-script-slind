b8a2c24fe00fde772bac731cec5599e4a8c19aba
Slind11 <vadim.tyan.2015@mail.ru>
2023-09-23 05:05:03 +0300

    доделал функцию которая делает бэкап в зависимости от  текущего дня недели или месяца.

--- backup-script.sh
+++ backup-script.sh
@@ -42,8 +42,35 @@ echo "Архиватор tar установлен."
 
 generate_backup_filename() {
     local Backup_directory="$1"
-    local Backup_type=$"$2"
-    local Current_data=$(date +"%Y%m%d_%H%M%S")
+    local Backup_type="$2"
+    local Current_date=$(date +"%Y%m%d_%H%M%S")
     echo "${Backup_type}_Backup_${Current_date}_$(basename "$Backup_directory").tar.gz"
 }
+
+func_backup() {
+   local Backup_directory="$1"
+   local Backup_save="$2"
+   local Backup_type="$3"
+   local Backup_filename=$(generate_backup_filename "$Backup_directory" "$Backup_type")
+   
+   tar -czf "$Backup_save/$Backup_filename" -C "$Backup_directory" .
+
+   echo "Бэкап успешно завершен в: $Backup_save с именем $Backup_filename"
+}
+
+# Определение текущего дня недели
+Current_day=$(date '+%w')
+
+
+# Создание бэкапа в зависимости от текущего дня недели
+if [ "$Current_day" -eq 0 ]; then
+    func_backup "$Backup_directory" "$Backup_save" "weekly"
+elif [ "$(date '+%-d')" -eq 1 ]; then
+    func_backup "$Backup_directory" "$Backup_save" "monthly"
+elif [ "$(date '+%-m')" -eq 12 ] && [ "$(date '+%-d')" -eq 31 ]; then
+    func_backup "$Backup_directory" "$Backup_save" "yearly"
+else
+    func_backup "$Backup_directory" "$Backup_save" "daily"
+fi
+
 
